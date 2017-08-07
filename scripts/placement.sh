5999f70a5d1dbd98104300e7b579aedb688c1a7b
Tim Edwards <tim@opencircuitdesign.com>
2017-08-06 22:31:47 -0400

    Corrected error in which for reasons that I don't understand, the LEF file DIRECTION value gets an extra space at the end and then fails subsequent comparisons.  Although I don't see how the regexp fails, it was easy enough to use the Tcl "string trim" command to remove whitespace.

--- scripts/placement.sh
+++ scripts/placement.sh
@@ -481,6 +481,16 @@ if ($makedef == 1) then
       echo "" >> ${synthlog}
 
       cd ${synthdir}
+
+      #------------------------------------------------------------------
+      # Copy the original rtl.v and rtlnopwr.v for use in comparison of
+      # pre- and post-placement netlists.
+      #------------------------------------------------------------------
+
+      echo "Copying ${rootname}.rtl.v and ${rootname}.rtlnopwr.v to backups"
+      cp ${rootname}.rtl.v ${rootname}_synth.rtl.v
+      cp ${rootname}.rtlnopr.v ${rootname}_synth.rtlnopwr.v
+
       echo "Running blif2Verilog." |& tee -a ${synthlog}
       ${bindir}/blif2Verilog -c -v ${vddnet} -g ${gndnet} \
 		${rootname}_anno.blif > ${rootname}.rtl.v
